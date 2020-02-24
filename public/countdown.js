56d734deab037dd85b25c703e15cb4c7ee1ec690
niels <niels.overkamp@gmail.com>
2020-02-25 00:07:36 +0100

    Initial simple countdown page
    Features:
    * Countdown in hours/minutes/seconds
    * Map background

---
+++ public/countdown.js
@@ -0,0 +1,13 @@
+
+const countdownTo = moment.tz("2020-05-18T20:55:00", "Europe/Amsterdam");
+const userTimezone = moment.tz.guess(true);
+
+
+
+window.onload = function() {
+    const countdown = document.querySelector("#countdown");
+    setInterval(function () {
+        const duration = moment.duration(countdownTo.diff(moment()));
+        countdown.textContent = `${Math.floor(duration.asDays())} days ${duration.hours()} hours ${duration.seconds()} seconds`;
+    }, 1000)
+};
